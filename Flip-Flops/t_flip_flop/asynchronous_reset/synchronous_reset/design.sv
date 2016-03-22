08228d7055b1e66ffcb1f42512885128db4e37fa
Shashank Singh <shashanksi2009@gmail.com>
2016-03-22 12:26:04 +0530

    Create design.sv

---
+++ Flip-Flops/t_flip_flop/asynchronous_reset/synchronous_reset/design.sv
@@ -0,0 +1,14 @@
+module t_flip_flop(
+  output q,
+  input t,
+  input reset,
+  input clk);
+  reg q;
+  always @( posedge(clk))
+    begin
+      if( reset == 0 )
+        q<=0;
+      else if(t)
+        q<=~q;
+    end
+endmodule
