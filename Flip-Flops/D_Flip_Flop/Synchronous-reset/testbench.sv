145713d240a82b07c607eae128334fee6ef38620
Shashank Singh <shashanksi2009@gmail.com>
2016-03-22 11:58:15 +0530

    Create testbench.sv

---
+++ Flip-Flops/D_Flip_Flop/Synchronous-reset/testbench.sv
@@ -0,0 +1,21 @@
+module tb();
+  wire q;
+  reg d , reset , clk ;
+  d_flip_flop dev(q,d,reset,clk);
+  initial
+    begin
+      $dumpvars(1,dev) ;
+      $dumpfile("dump.vcd") ;
+      reset=1;
+      clk = 0;
+      d = 0;
+      #2 clk = 1;
+      #2 clk = 0;
+      #2 d = 1;
+      #2 clk = 1;
+      #2 clk = 0;
+      #2 reset = 0;
+      #2 clk = 1;
+      #2 $finish ;
+    end
+endmodule
